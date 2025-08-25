17999eedee72864e471440c4dcea636d8b5e5c91
kauanAfonso <kauanafon3@gmail.com>
2025-08-25 08:20:03 -0300

    'Arquivos'

---
+++ Frontend/src/Componentes/BarraNavegacao.jsx
@@ -0,0 +1,11 @@
+export function BarraNavegacao(){
+    return(
+        <nav>
+            <ul>
+                <li>Cadastro de Usuário</li>
+                <li>Cadasro de Tarefa</li>
+                <li>Gerenciamento de Tarefas</li>
+            </ul>
+        </nav>
+    )
+}
