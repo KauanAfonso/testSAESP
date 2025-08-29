57d04fa5abaa589e59d4f70c76261960e2f49b31
kauanAfonso <kauanafon3@gmail.com>
2025-08-29 09:06:49 -0300

    Aula de front

--- Frontend/src/paginas/CadUsuario.jsx
+++ Frontend/src/paginas/CadUsuario.jsx
@@ -1,31 +1,55 @@
 import axios from 'axios';
-import { Form } from 'react-router';
 import { useForm } from 'react-hook-form';
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { any, z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-//validação de formulario
-const  schemaCadUsuario = z.object({
+//validação de formulário
+const schemaCadUsuario = z.object({
     nome: z.string()
-        .min(1,'Insira ao menos 1 caractere')
+        .min(1, 'Insira ao menos 1 caractere')
         .max(30, 'Insira até 30 caracteres'),
-    email:z.string()
-        .min(1, "Insira seu email")
-        .max(30, "Insira um enderço de email com até 30 caracteres")
-        .email('Formato  de email invalido'),
-    
+    email: z.string()
+        .min(1, 'Insira seu email')
+        .max(30, 'Insira um endereço de email com até 30 carateres')
+        .email("Formato de email invalido"),
 })
 
-export function CadUsuario(){
-    return(
-        <form>
+
+export function CadUsuario() {
+
+    const {
+        resister,
+        handleSubmit,
+        formState: { errors },
+        reset
+    } = useForm({
+        resolver: zodResolver(schemaCadUsuario)
+    })
+
+    async function obterdados(data) {
+        console.log('dados informados pelo user:', data)
+
+        try {
+            await axios.post("http://127.0.0.1:8000/usuario/", data);
+            alert("Usuário cadastrado com sucesso");
+            reset(); //limpeza do formulario
+        } catch (error) {
+            alert("È não rolou na proxima talvez");
+            console.log("Erros", error)
+        }
+    }
+
+
+
+    return (
+        <form className="formularios">
             <h2>Cadastro do Usuário</h2>
-            
+
             <label>Nome:</label>
-            <input type="text" placeholder='Nome do usuário' required/>
+            <input type='text' placeholder='Jose da Silva' required />
 
             <label>E-mail</label>
-            <input type="email"  placeholder='email@email.com' required/>
+            <input type='email' placeholder='email@email.com' required />
 
             <button type='submit'>Cadastrar</button>
 
